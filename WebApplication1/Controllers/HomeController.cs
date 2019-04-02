56ba1adb5cd67c774815d0c812ad17433b175fc2
Hemant Shelar <hemant.shelar@gmail.com>
2019-04-03 09:43:37 +1100

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly UserManager<MyUser> userManager = null;
+		//private readonly SignInManager<MyUser> signInManager = null;
+
+		public HomeController(UserManager<MyUser> userManager)
+		{
+			this.userManager = userManager;
+			
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[Authorize]
+		public IActionResult About()
+		{
+			ViewData["Message"] = "Your application description page.";
+
+			return View();
+		}
+
+		public IActionResult Contact()
+		{
+			ViewData["Message"] = "Your contact page.";
+
+			return View();
+		}
+
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+
+		public IActionResult Register()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Register(RegisterModel registerModel)
+		{
+			var identityResult = await this.userManager.CreateAsync(new MyUser()
+			{
+				Id = Guid.NewGuid().ToString(),
+				UserName = registerModel.UserName,				
+				PasswordHash = registerModel.Password
+			},registerModel.Password);
+
+			if (identityResult.Succeeded)
+				return View();
+			else
+			{
+				int id = 1;
+				foreach (var item in identityResult.Errors)
+				{
+					ModelState.AddModelError(id.ToString(), item.Description);
+					id++;
+				}
+				return View();
+			}
+		}
+
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+
+		[HttpPost]
+		public async Task<IActionResult> Login(LoginModel loginModel)
+		{
+			var user = await this.userManager.FindByIdAsync(loginModel.UserName);
+			if (  user != null  )
+			{
+				bool bValue = await this.userManager.CheckPasswordAsync(user, loginModel.Password);
+				if ( bValue == true )
+				{
+					ClaimsIdentity claimsIdentity = new ClaimsIdentity();
+					await HttpContext.SignInAsync("cookies",new ClaimsPrincipal(claimsIdentity));
+				}
+			}
+		}
+	}
+}
---
+++ WebApplication1/Models/MyStore.cs
@@ -0,0 +1,119 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+	public class MyStore : IUserStore<MyUser>,IUserPasswordStore<MyUser>
+	{
+		public DbConnection GetConnection()
+		{
+			string connectionString = @"Data Source=(localdb)\mssqllocaldb;Initial Catalog=MyDb;Integrated Security=True";
+			SqlConnection conn = new SqlConnection(connectionString);
+			conn.Open();
+			return conn;
+		}
+		public async Task<IdentityResult> CreateAsync(MyUser user, CancellationToken cancellationToken)
+		{
+			using (var conn = GetConnection())
+			{
+				string insertQuery = $"insert into MyUsers(id,username,NormalizedUserName,passwordhash) values ('{user.Id}','{user.UserName}','{user.NormalizedUserName}','{user.PasswordHash}')";
+				await conn.ExecuteAsync(insertQuery);
+			}
+			return IdentityResult.Success;
+		}
+
+		public Task<IdentityResult> DeleteAsync(MyUser user, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Dispose()
+		{
+			Console.WriteLine("disposed called");
+		}
+
+		public async Task<MyUser> FindByIdAsync(string userId, CancellationToken cancellationToken)
+		{
+			MyUser myUser = null;
+			using (var conn = GetConnection())
+			{
+				var r = await conn.QueryFirstOrDefaultAsync<MyUser>($"SELECT * from MyUsers where Id = '{userId}'");
+			}
+			return myUser;
+		}
+
+		public async Task<MyUser> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken)
+		{
+			MyUser myUser = null;
+			using (var conn = GetConnection())
+			{
+				var r = await conn.QueryFirstOrDefaultAsync<MyUser>($"SELECT * from MyUsers where NormalizedUserName = '{normalizedUserName}'");
+			}
+			return myUser;
+		}
+
+		public Task<string> GetNormalizedUserNameAsync(MyUser user, CancellationToken cancellationToken)
+		{
+			return Task.FromResult(user.NormalizedUserName);
+		}
+
+		public Task<string> GetUserIdAsync(MyUser user, CancellationToken cancellationToken)
+		{
+			return Task.FromResult(user.Id);
+		}
+
+		public Task<string> GetUserNameAsync(MyUser user, CancellationToken cancellationToken)
+		{
+			return Task.FromResult(user.UserName);
+		}
+
+		public Task SetNormalizedUserNameAsync(MyUser user, string normalizedName, CancellationToken cancellationToken)
+		{
+			user.NormalizedUserName = normalizedName;
+			return Task.CompletedTask;
+
+		}
+
+
+		public Task SetUserNameAsync(MyUser user, string userName, CancellationToken cancellationToken)
+		{
+			user.UserName = userName;
+			return Task.CompletedTask;
+		}
+
+		public async Task<IdentityResult> UpdateAsync(MyUser user, CancellationToken cancellationToken)
+		{
+			using (var conn = GetConnection())
+			{
+				string insertQuery = $"UPDATE MyUsers set id = {user.Id}, UserName = {user.UserName}, " +
+					$"NormalizedUserName = {user.NormalizedUserName},PasswordHash= {user.PasswordHash})";
+				await conn.ExecuteAsync(insertQuery);
+			}
+			return IdentityResult.Success;
+		}
+
+		public Task SetPasswordHashAsync(MyUser user, string passwordHash, CancellationToken cancellationToken)
+		{
+			user.PasswordHash = passwordHash;
+			return Task.CompletedTask;
+		}
+
+		public Task<string> GetPasswordHashAsync(MyUser user, CancellationToken cancellationToken)
+		{
+			var result = user.PasswordHash;
+			return Task.FromResult(result);
+		}
+
+		public Task<bool> HasPasswordAsync(MyUser user, CancellationToken cancellationToken)
+		{
+			return Task.FromResult(user.PasswordHash != null);
+		}
+	}
+}
